5a9b24eeefb1f3c9047d2f486a24e63fa3801d8b
abyad-tif <lapajung50@gmail.com>
2025-11-27 10:52:29 +0800

    minor update (adding home)

--- app/dashboard/sidebar.tsx
+++ app/dashboard/sidebar.tsx
@@ -40,6 +40,49 @@ export default function Sidebar() {
           <ul className="space-y-2">
             <li>
               <a
+                href="/dashboard"
+                className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+              >
+                <svg
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                >
+                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <path
+                      d="M3 8.976C3 4.05476 4.05476 3 8.976 3H15.024C19.9452 3 21 4.05476 21 8.976V15.024C21 19.9452 19.9452 21 15.024 21H8.976C4.05476 21 3 19.9452 3 15.024V8.976Z"
+                      stroke="#323232"
+                      stroke-width="2"
+                    ></path>{" "}
+                    <path
+                      d="M21 9L3 9"
+                      stroke="#323232"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                    <path
+                      d="M9 21L9 9"
+                      stroke="#323232"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    ></path>{" "}
+                  </g>
+                </svg>
+                <span className="ml-3">Home</span>
+              </a>
+            </li>
+            <li>
+              <a
                 href="#"
                 className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
